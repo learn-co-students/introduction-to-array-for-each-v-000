d178f703a6d50bb5fead1559ccf98ca6f1d170ca
Eric Loos <ericloos00@gmail.com>
2017-02-09 15:48:31 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
+ 
+ var animals = ["dog", "fish", "cat"]
+ 
+ 
+ function doToElementsInArray(array, callback)  {
+  array.forEach(callback)
+ }
+
