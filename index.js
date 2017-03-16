e95908513cd5e7bc103d92d2c9817aac244210e7
neener <janinemrosen@gmail.com>
2017-03-16 02:15:07 -0400

    done

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-// var evens = [0, 2, 4, 6, 8, 10]
+var evens = [0, 2, 4, 6, 8, 10]
  
 // for (var i = 0, l = evens.length; i < l; i++) {
 //   console.log(`${evens[i]} is not odd!`)
 // }
 
 // evens.forEach(even => {
-// 	debugger
 //   console.log(`${even} is not odd!`)
 // })
 
+// evens.forEach((even, index, array) => {
+//   debugger
+//   console.log(`${even} is not odd!`)
+// })
 
 // function square(n) {
 //   console.log(n * n)
 // }
 
+// evens.forEach(square)
 
+function doToEvens(callback) {
+  evens.forEach(callback)
+}
 
-// function doToEvens(callback) {
-//   evens.forEach(callback)
-// }
-
-// function doToElementsInArray(array, callback) {
-//   array.forEach(callback)
-// }
+function doToElementsInArray(array, callback) {
+  array.forEach(callback)
+}
 
 function changeCompletely(element, index, array) {
   array[index] = (Math.random() * 100).toString() + '!!!'
@@ -32,5 +35,5 @@ var animals = ["dog", "fish", "cat"]
  
 doToElementsInArray(animals, changeCompletely)
  
-// log out animals -- pretty cool, right?
+// // log out animals -- pretty cool, right?
 console.log(animals)
