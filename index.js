d64458f0be47a0ac7408ebc5f5257c3522567a19
pacosta613 <pedro.acosta613@gmail.com>
2017-03-21 14:41:58 -0400

    Done

---
+++ index.js
@@ -0,0 +1,10 @@
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + "!!!"
+}
+
+function doToElementsInArray(array, callback) {
+  array.forEach(callback)
+}
+
+var animals = ["dog", "fish", "cat"]
+doToElementsInArray(animals, changeCompletely)
