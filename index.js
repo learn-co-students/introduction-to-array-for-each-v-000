7ed9cee68a008086c181c2f4509f0be6f20a4cce
mirandalily <mirandaraymond@me.com>
2017-09-12 15:50:43 -0700

    set up functions

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Add your doToElementsInArray() function here:
 
+function doToElementsInArray(array, callback){
+
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely(element, index, array){
+  
+}
