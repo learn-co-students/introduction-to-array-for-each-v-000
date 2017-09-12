11db7b0b5e910887c167f3f2a24aa5f319738b0a
mirandalily <mirandaraymond@me.com>
2017-09-12 15:51:23 -0700

    3 tests passing

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Add your doToElementsInArray() function here:
 
 function doToElementsInArray(array, callback){
-
+  array.forEach(callback);
 }
 
 // Add your changeCompletely() function here:
 function changeCompletely(element, index, array){
-  
+
 }
