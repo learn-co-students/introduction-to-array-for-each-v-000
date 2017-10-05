c683fee290cdcbbce2fe274a7c1da8f424a603f6
James Fernandez <jffernan@comcast.net>
2017-10-05 12:44:32 -0700

    Re-do & added pseudocode // comments

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-// Add your doToElementsInArray() function here:
+/* 
+1. Declare a function named doToElementsInArray.
+2. Function accepts parameter named array, callback, whose value is array, callback.
+3. When call function, execute callback.
+*/
 function doToElementsInArray(array, callback) {
   array.forEach(callback);
 }
 
-// Add your changeCompletely() function here:
+/* 
+1. Declare a function named changeCompletely.
+2. Function accepts parameter named element, index, array, whose value is element, index, array.
+3. When call function, execute callback to change array.
+*/
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
